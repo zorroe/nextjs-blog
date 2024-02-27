da3986542470007d288a9d86479735a897229c91
lixuan <1318753541@qq.com>
2024-02-27 20:53:05 +0800

    update

---
+++ pages/posts/first-post.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
+  )
+}
